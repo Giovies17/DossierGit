875207df30c2c635698ee6118994747e559c7e17
Serge <81445456+Giovies17@users.noreply.github.com>
2023-11-16 16:57:52 +0100

    Add files via upload

---
+++ exercice.js/js/script.js
@@ -0,0 +1,48 @@
+// Var $myVar, var VAR car une contient le symbole $ qu'on utilise dans le PHP ainsi que l'autre il y'a une Majuscules
+//exercice java
+//let x = 20;
+//let y = -10;
+//let moy = x + y / 2;
+//console.log("let x " + x );
+//console.log("let y " + y);
+//console.log ("moy" +  y/x)
+
+// exercice 2
+
+//let bjr = bonjour;
+//let str 1
+//var myVar;
+
+//Nombre
+let x = 10;
+let y = -10;
+let moy = (x + y) / 2; //2,5
+moy = (x + y + moy) / 3;
+
+//String
+let bjr = "Bonjour";
+bjr = bjr + "_le monde";
+
+let str1 = "Ceci est_: '";
+let str2 = "Ceci est une double quote : '";
+let str3 = "Ceci est une quote suivi d'une double : '\"";
+let str4 = "\\";
+
+//4
+
+let nbr = 200,
+  texte = "200",
+  decimal = 2.5,
+  bool = false;
+
+nbr = nbr + 1;
+nbr++;
+++nbr;
+
+//texte = texte + "-exit";
+//bool = !bool;
+//decimal = decimal + nbr;
+//nbr + texte = ("200200");
+//texte + nbr = ("200200");
+//nbr + parseInt (texte) = 400;
+//nbr*texte = NaN 
