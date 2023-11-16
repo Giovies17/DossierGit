ce78d10a64b96eed796593cd45374283ce951241
Serge <81445456+Giovies17@users.noreply.github.com>
2023-11-16 23:54:04 +0100

    Add files via upload

---
+++ detruitlephp/js/script.js
@@ -0,0 +1,32 @@
+let x = 20;
+let y = -20;
+let moy = x + y / 2;
+let moyenne = x + y + moy / 3;
+
+//String
+//3)
+let bjr = "bonjour";
+console.log("bonjour" + " " + "le monde");
+let str1 = "Ceci est une quote : '";
+let str2 = "Ceci est une double quote \"";
+let str3 = "Ceci est une quote suivie d'une double quote '\"";
+let str4 = "Cette variable contient u\n anti slash"
+
+//4
+
+let nbr = 200;
+let texte = ('200');
+let decimal = 2.5;
+let bool = false;
+// 
+// Demander à Ludo 
+nbr = nbr + 1;
+nbr++;
+//++nbr;;
+//let text = ("200" + "est une chaine de caractères");
+//bool = false;
+//bool = !false;
+decimal = decimal + nbr;
+console.log(nbr + texte + "puis de texte" + nbr);
+
+
